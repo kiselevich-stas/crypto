75a932884cbd96e8fb6db2603e9aedf641f15b29
satastast <78559077+kiselevich-stas@users.noreply.github.com>
2023-06-29 00:31:51 +0300

    feat: add new methods and new data

--- vue-crypto/src/main.js
+++ vue-crypto/src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+import 'swiper/swiper-bundle.css';
 createApp(App).use(store).use(router).mount('#app')
